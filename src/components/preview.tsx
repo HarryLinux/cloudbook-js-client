c34b49d297e8b804470c70f88f4c73a92abd4d15
Harold Fich <haroldfich@gmail.com>
2021-03-02 01:47:30 -0500

    improved error handling and displaying

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -3,36 +3,48 @@ import { useRef, useEffect } from "react";
 
 interface PreviewProps {
   code: string;
+  errorMessage: string;
 }
 
 const html = `
-        <html>
-            <head>
-              <style>html { background-color: white; }</style>
-            </head>
-            <body>
-                <div id="root"></div>
-                <script>
-                    window.addEventListener('message', (event) => {
-                        try {
-                            eval(event.data);
-                        } catch (err) {
-                            const root = document.querySelector('#root');
-                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-                            console.error(err);
-                        }
-                    }, false);
-                </script>
-            </body>
-        </html>
-    `;
+    <html>
+      <head>
+        <style>html { background-color: white; }</style>
+      </head>
+      <body>
+        <div id="root"></div>
+        <script>
+          const handleError = (err) => {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err);
+          };
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+          window.addEventListener('error', (event) => {
+            event.preventDefault();
+            handleError(event.error);
+          });
+
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              handleError(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
+
+const Preview: React.FC<PreviewProps> = ({ code, errorMessage }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, "*");
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, "*");
+    }, 50);
   }, [code]);
 
   return (
@@ -43,6 +55,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
         srcDoc={html}
         sandbox="allow-scripts"
       />
+      {errorMessage && <div className="preview-error">{errorMessage}</div>}
     </div>
   );
 };
